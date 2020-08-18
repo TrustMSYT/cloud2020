3fd933903cc715cf96bb621d22ca18241d88fd0a
unknown <992590075@qq.com>
2020-08-18 16:43:08 +0800

    Initial commit

---
+++ src/main/java/com/dustin/springcloud/cloud2020/Cloud2020Application.java
@@ -0,0 +1,13 @@
+package com.dustin.springcloud.cloud2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloud2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cloud2020Application.class, args);
+    }
+
+}
